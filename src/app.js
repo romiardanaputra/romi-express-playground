54ab5c0cb9f1a71cab827c746a13d940fa115a94
romiardanaputra <romiardana21@gmail.com>
2024-09-08 00:13:30 +0800

    feat: Add sample put and patch

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import connectDB from "../src/config/db.js";
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3333;
 
 // const startServer = async () => {
@@ -21,6 +23,7 @@ app.get("/", (req, res) => {
   res.status(201).send({ msg: "hello" });
 });
 
+// mockup data
 const users = [
   {
     id: 1,
@@ -39,6 +42,7 @@ const users = [
   },
 ];
 
+// get request
 app.get("/api/v1/users", (req, res) => {
   console.log(req.query);
   // res.send(users);
@@ -48,12 +52,28 @@ app.get("/api/v1/users", (req, res) => {
   if (filter && value) {
     return res.send(users.filter((user) => user[filter].includes(value)));
   }
-  if (!filter && !value) {
-    return res.send(users);
-  }
-  return res.status(400).send({ msg: "invalid query" });
+  return res.send(users);
 });
 
+// post request
+app.post("/api/v1/users", (req, res) => {
+  const { body } = req;
+  console.log(body);
+  const createUser = { id: users[users.length - 1].id + 1, ...body };
+  users.push(createUser);
+  return res.status(201).send(createUser);
+});
+
+// const testUser = {
+//   id: 4,
+//   name: "test",
+//   age: "30",
+// };
+
+// const result = { ...users[0] };
+// console.log(result);
+
+// get request with params
 app.get("/api/v1/users/:id", (req, res) => {
   const parseId = parseInt(req.params.id);
   if (isNaN(parseId)) {
@@ -69,4 +89,31 @@ app.get("/api/v1/users/:id", (req, res) => {
   return res.send(findUser);
 });
 
-app.use(express.json());
+// patch \\ use to update partial field
+app.patch("/api/v1/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  const parseId = parseInt(id);
+  if (isNaN(parseId)) return res.status(400).send({ msg: "invalid request" });
+  const userIdx = users.findIndex((user) => user.id === parseId);
+  if (userIdx === -1) return res.sendStatus(404);
+  users[userIdx] = { ...users[userIdx], ...body };
+  return res.sendStatus(200);
+});
+
+// put \\ use to update entire fields
+app.put("/api/v1/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  const parseId = parseInt(id);
+  if (isNaN(parseId)) return res.status(400).send({ msg: "invalid request" });
+  const userIdx = users.findIndex((user) => user.id === parseId);
+  if (userIdx === -1) return res.sendStatus(404);
+  users[userIdx] = { id: parseId, ...body };
+  return res.sendStatus(200);
+});
+// delete
