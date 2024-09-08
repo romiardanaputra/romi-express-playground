d39d577d7a52b6bad85f207450ae9d6975336df0
romiardanaputra <romiardana21@gmail.com>
2024-09-08 18:25:08 +0800

    feat: Add sample data

---
+++ src/data/index.js
@@ -0,0 +1,18 @@
+// mockup data
+export const users = [
+  {
+    id: 1,
+    name: "romi",
+    age: "30",
+  },
+  {
+    id: 2,
+    name: "joen jong seon",
+    age: "30",
+  },
+  {
+    id: 3,
+    name: "dewi",
+    age: "30",
+  },
+];
