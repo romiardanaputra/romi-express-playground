81d496f469672897ff2fc2f98e5e9aa62ddb3736
romiardanaputra <romiardana21@gmail.com>
2024-09-09 21:36:48 +0800

    feat: Add product routes domain

---
+++ src/routes/products.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+const router = Router();
+
+export const product = [
+  {
+    id: 1,
+    name: "product 1",
+    price: "100",
+  },
+  {
+    id: 2,
+    name: "product 2",
+    price: "200",
+  },
+  {
+    id: 3,
+    name: "product 3",
+    price: "300",
+  },
+];
+
+router.get("/api/v1/products", (req, res) => {
+  return res.send(product).status(200);
+});
