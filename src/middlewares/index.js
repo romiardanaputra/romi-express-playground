82a69646b4bd3a82f365a98198b98b1966372ef1
romiardanaputra <romiardana21@gmail.com>
2024-09-09 20:09:07 +0800

    feat(middleware): Add middleware for resolveUserById

---
+++ src/middlewares/index.js
@@ -0,0 +1,13 @@
+export const resolveUserIdx = (req, res, next) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  const parseId = parseInt(id);
+  if (isNaN(parseId)) return res.status(400).send({ msg: "invalid request" });
+  const userIdx = users.findIndex((user) => user.id === parseId);
+  if (userIdx === -1) return res.sendStatus(404);
+  req.userIdx = userIdx;
+  console.log("result inside resolveUserIdx", userIdx);
+  next();
+};
