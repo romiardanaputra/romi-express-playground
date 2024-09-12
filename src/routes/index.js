e907646ffd4cb38d0858bfcc70b83ccfd5a80192
romiardanaputra <romiardana21@gmail.com>
2024-09-12 12:24:16 +0800

    refactor(route): Refactor base route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
 import usersRouter from "../routes/users.js";
 import productsRouter from "../routes/products.js";
+import authRouter from "../routes/auth.js";
+import cartsRouter from "../routes/carts.js";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+  console.log(req.session);
+  console.log(req.session.id);
+  console.log((req.session.visited = true));
+  req.session.visited = true;
+  res.cookie("setCookie", "cookieValue", { maxAge: 30000, signed: true });
+  res.status(201).send({ msg: "hello" });
+});
+
 router.use(usersRouter);
 router.use(productsRouter);
+// router.use(authRouter);
+router.use(cartsRouter);
 
 export default router;
