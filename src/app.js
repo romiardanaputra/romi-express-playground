820a54450ecab8b49dd388122f01a61a37413907
romiardanaputra <romiardana21@gmail.com>
2024-09-15 09:16:16 +0800

    feat: implement discord oauth

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ import cookieParser from "cookie-parser";
 import session from "express-session";
 import { loggingMiddleware } from "./middlewares/index.js";
 import passport from "passport";
-import "./strategies/local-strategy.js";
+// import "./strategies/local-strategy.js";
 import mongoose from "mongoose";
 import MongoStore from "connect-mongo";
+import "./strategies/discord-strategy.js";
 
 const app = express();
 
@@ -37,28 +38,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(routes);
-app.post("/api/v1/auth", passport.authenticate("local"), (req, res) => {
-  res.sendStatus(200);
-});
 
-app.get("/api/v1/auth/status", (req, res) => {
-  console.log(`Inside auth/status endpoint`);
-  console.log(req.user);
-  console.log(req.session);
-  console.log(req.sessionID);
-  return req.user ? res.send(req.user) : res.sendStatus(401);
-});
-
-app.post("/api/v1/auth/logout", (req, res) => {
-  if (!req.user) return res.sendStatus(401);
-  req.logout((err) => {
-    if (err) return res.sendStatus(400);
-    res.send(200);
-  });
-  console.log(`what is inside req.user? ${req.user}`);
-});
+app.get("/api/auth/discord", passport.authenticate("discord"));
+app.get(
+  "/api/auth/discord/redirect",
+  passport.authenticate("discord"),
+  (req, res) => {
+    res.sendStatus(200);
+  }
+);
 
 const PORT = 3333;
 app.listen(PORT, () => {
   console.info(`server running on port ${PORT}`);
 });
+
+// const clientId = 1284366855284457522
+// const clientSecret = Nq1jfOb1qkGwnHJr1BY09TwVKEH5Ks9X
+// const redirectUri = "http://localhost:3000/api/auth/discord/redirect"
