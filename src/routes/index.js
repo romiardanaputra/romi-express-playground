8a6f8fd52085f4ca646f8007e0cded15de66936d
romiardanaputra <romiardana21@gmail.com>
2024-09-15 09:16:48 +0800

    feat: add route 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import usersRouter from "../routes/users.js";
 import productsRouter from "../routes/products.js";
 import authRouter from "../routes/auth.js";
 import cartsRouter from "../routes/carts.js";
+import authRouter2 from "../routes/auth2.js";
 
 const router = Router();
 
@@ -19,5 +20,6 @@ router.use(usersRouter);
 router.use(productsRouter);
 // router.use(authRouter);
 router.use(cartsRouter);
+router.use(authRouter2);
 
 export default router;
